ef735bd3fe6307229c4da004430ad03cef64f4bc
Anton <danilovanton724@gmail.com>
2024-02-28 13:36:33 +0100

    Initial Commit

---
+++ Pegglo Plunge/Pegglo_PlungeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pegglo_PlungeApp.swift
+//  Pegglo Plunge
+//
+//  Created by Anton on 28/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Pegglo_PlungeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
